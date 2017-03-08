1e517350a191623e97b3102788925e7b108c66af
marc094 <marcmoreno2@gmail.com>
2017-03-08 17:04:29 +0100

    imagenes

--- main.cpp
+++ main.cpp
@@ -53,7 +53,7 @@
 #define SHIP_SPEED 3
 #define NUM_SHOTS 32
 #define SHOT_SPEED 5
-#define MAX_ROCKS 8
+#define MAX_ROCKS 16
 
 
 struct rectangle {
@@ -76,6 +76,7 @@ struct rock {
 	bool alive;
 	int rotation;
 	int speed;
+	int angle = 0;
 };
 
 struct globals
@@ -85,6 +86,7 @@ struct globals
 	SDL_Texture* background = nullptr;
 	SDL_Texture* ship = nullptr;
 	SDL_Texture* shot = nullptr;
+	SDL_Texture* asteroid = nullptr;
 	int background_width = 0;
 	int ship_x = 0;
 	int ship_y = 0;
@@ -98,6 +100,79 @@ struct globals
 } g; // automatically create an insteance called "g"
 
 // ----------------------------------------------------------------
+bool pointInRectangle(int x, int y, rectangle rect) {
+	if (x < rect.w + rect.x && x > rect.x)
+	{
+		if (y < rect.h + rect.y && y > rect.y)
+			return true;
+	}
+	return false;
+}
+
+bool squareInCirlce(circle circl, rectangle rect) {
+	for (int i = 0; i < 4; i++) {
+		switch (i) {
+		case 0:
+			if (sqrt(pow(rect.x - circl.x, 2) + pow(rect.y - circl.y, 2)) <= circl.radius) {
+				return true;
+			}
+			break;
+		case 1:
+			if (sqrt(pow(rect.x + rect.w - circl.x, 2) + pow(rect.y - circl.y, 2)) <= circl.radius) {
+				return true;
+			}
+			break;
+		case 2:
+			if (sqrt(pow(rect.x + rect.w - circl.x, 2) + pow(rect.y + rect.h - circl.y, 2)) <= circl.radius) {
+				return true;
+			}
+			break;
+		case 3:
+			if (sqrt(pow(rect.x - circl.x, 2) + pow(rect.y + rect.h - circl.y, 2)) <= circl.radius) {
+				return true;
+			}
+			break;
+		}
+	}
+	return false;
+}
+
+bool checkCollision(rock rock, projectile proj)
+{
+	if (pointInRectangle(rock.circl.x, rock.circl.y, proj.rect))
+		return true;
+	if (squareInCirlce(rock.circl, proj.rect))
+		return true;
+	return false;
+}
+
+void initNextRock(int index) {
+	rock* prev_rock = &g.rocks[index];
+
+	if (prev_rock->circl.radius >= 25) {
+		rock* new_rock = nullptr;
+		for (int i = index; i < MAX_ROCKS; i++) {
+			if (!g.rocks[i].alive) {
+				new_rock = &g.rocks[i];
+				break;
+			}
+		}
+		new_rock->circl.radius = prev_rock->circl.radius * 0.75;
+		prev_rock->circl.radius = new_rock->circl.radius;
+		new_rock->circl.x = prev_rock->circl.x;
+		new_rock->circl.y = prev_rock->circl.y + prev_rock->circl.radius;
+		prev_rock->circl.y = prev_rock->circl.y - prev_rock->circl.radius;
+		new_rock->alive = true;
+		prev_rock->rotation *= 2;
+		new_rock->rotation = prev_rock->rotation;
+		prev_rock->speed += 1;
+		new_rock->speed = prev_rock->speed;
+	}
+	else {
+		prev_rock->alive = false;
+	}
+}
+
 void Start()
 {
 	SDL_Init(SDL_INIT_EVERYTHING);
@@ -111,6 +186,7 @@ void Start()
 	g.background = SDL_CreateTextureFromSurface(g.renderer, IMG_Load("assets/background.png"));
 	g.ship = SDL_CreateTextureFromSurface(g.renderer, IMG_Load("assets/ship.png"));
 	g.shot = SDL_CreateTextureFromSurface(g.renderer, IMG_Load("assets/shot.png"));
+	g.asteroid = SDL_CreateTextureFromSurface(g.renderer, IMG_Load("assets/rock.png"));
 	SDL_QueryTexture(g.background, nullptr, nullptr, &g.background_width, nullptr);
 
 	// Create mixer --
@@ -129,7 +205,7 @@ void Start()
 	g.rocks[0].alive = true;
 	g.rocks[0].circl.x = SCREEN_WIDTH;
 	g.rocks[0].circl.y = SCREEN_HEIGHT / 2;
-	g.rocks[0].speed = 2;
+	g.rocks[0].speed = 1;
 	g.rocks[0].circl.radius = 100;
 	g.rocks[0].rotation = 1;
 			
@@ -207,7 +283,7 @@ void MoveStuff()
 
 		g.shots[g.last_shot].alive = true;
 		g.shots[g.last_shot].rect.x = g.ship_x + 32;
-		g.shots[g.last_shot].rect.y = g.ship_y;
+		g.shots[g.last_shot].rect.y = g.ship_y + 32 - g.shots[g.last_shot].rect.h/2;
 		++g.last_shot;
 	}
 
@@ -224,10 +300,21 @@ void MoveStuff()
 				if (g.rocks[j].alive) {
 					if (checkCollision(g.rocks[j], g.shots[i])) {
 						g.shots[i].alive = false;
-						initNextRock(g.rocks, j);
+						initNextRock(j);
 					}
 				}
 			}
+		}
+	}
+
+	for (int i = 0; i < MAX_ROCKS; ++i)
+	{
+		if (g.rocks[i].alive)
+		{
+			g.rocks[i].circl.x -= g.rocks[i].speed;
+			g.rocks[i].angle += g.rocks[i].rotation;
+			if (g.rocks[i].circl.x + g.rocks[i].circl.radius < 0)
+				g.rocks[i].circl.x = SCREEN_WIDTH + g.rocks[i].circl.radius;
 		}
 	}
 }
@@ -262,62 +349,18 @@ void Draw()
 		}
 	}
 
-	// Finally swap buffers
-	SDL_RenderPresent(g.renderer);
-}
-
-bool pointInRectangle(int x, int y, rectangle rect) {
-	if (x < rect.w + rect.x && x > rect.x)
+	// Draw rocks --
+	for (int i = 0; i < MAX_ROCKS; ++i)
 	{
-		if (y < rect.h + rect.y && y > rect.y)
-			return true;
-	}
-	return false;
-}
-
-bool squareInCirlce(circle circl, rectangle rect) {
-	for (int i = 0; i < 4; i++) {
-		switch (i) {
-		case 0:
-			if (sqrt(pow(rect.x - circl.x, 2) + pow(rect.y - circl.y, 2)) <= circl.radius) {
-				return true;
-			}
-			break;
-		case 1:
-			if (sqrt(pow(rect.x + rect.w - circl.x, 2) + pow(rect.y - circl.y, 2)) <= circl.radius) {
-				return true;
-			}
-			break;
-		case 2:
-			if (sqrt(pow(rect.x + rect.w - circl.x, 2) + pow(rect.y + rect.h - circl.y, 2)) <= circl.radius) {
-				return true;
-			}
-			break;
-		case 3:
-			if (sqrt(pow(rect.x - circl.x, 2) + pow(rect.y + rect. h - circl.y, 2)) <= circl.radius) {
-				return true;
-			}
-			break;
+		if (g.rocks[i].alive)
+		{
+			target = { g.rocks[i].circl.x - g.rocks[i].circl.radius, g.rocks[i].circl.y - g.rocks[i].circl.radius, g.rocks[i].circl.radius*2, g.rocks[i].circl.radius*2 };
+			SDL_RenderCopyEx(g.renderer, g.asteroid, nullptr, &target, g.rocks[i].angle, nullptr, SDL_FLIP_NONE);
 		}
 	}
-	return false;
-}
 
-bool checkCollision(rock rock, projectile proj)
-{
-	if (pointInRectangle(rock.circl.x, rock.circl.y, proj.rect))
-		return true;
-	if (squareInCirlce(rock.circl, proj.rect))
-		return true;
-	return false;
-}
-
-void initNextRock(rock* array_rocks, int index) {
-	rock* prev_rock = &array_rocks[index];
-	int next_dead = 0;
-	for (int i = 0; i < MAX_ROCKS; i++) {
-
-	}
+	// Finally swap buffers
+	SDL_RenderPresent(g.renderer);
 }
 
 // ----------------------------------------------------------------
